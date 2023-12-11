ace91c2ae9e7cd2ed92d798db63a25668b63d113
JeanFDev <jeanjfra1@gmail.com>
2023-12-11 20:56:17 -0300

    update json

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+function updateProfileInfo(profileData){
+    const photo = document.getElementById('profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name
+    const name = document.getElementById('profile.name')
+    name.innerText = profileData.name
+    
+    const job = document.getElementById('profile.job')
+    job.innerText = profileData.job
+
+    const location = document.getElementById('profile.location')
+    location.innerText = profileData.location
+
+    const phone = document.getElementById('profile.phone')
+    phone.innerText = profileData.phone
+    phone.href = `tel:$(profileData.phone)`
+
+    const email = document.getElementById('profile.mail')
+    email.innerText = profileData.mail
+    email.href = `mailto:$(profileData.mail)`
+
+
+}
+
+(async () =>{
+    const profileData = await fetchProfileData()
+    updateProfileInfo(profileData)
+})()
